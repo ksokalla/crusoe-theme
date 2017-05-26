0b59e4cabe5bbbffab05b205eec6479ba754ba78
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-05-26 19:48:36 +0200

    footer init

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
-
+<section id="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+            
+            </div>            
+            <div class="col-lg-4 col-sm-6">
+            
+            </div>            
+            <div class="col-lg-4 col-sm-6">
+            
+            </div>
+        </div>
+    </div>
+</section>
 
 
 

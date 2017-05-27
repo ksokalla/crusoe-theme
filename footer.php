acb3c0a1d6ff8876a476d9826ebdfc3d1ff809a2
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-05-27 18:22:22 +0200

    pros init

--- footer.php
+++ footer.php
@@ -2,20 +2,20 @@
     <div class="container-fluid">
         <div class="row">
             <div id="footer-bar">
-                <div class="col-lg-4 col-sm-4">
+                <div class="col-lg-4 col-sm-4 footer-headers">
                 <h2>Contact Us</h2>
                 <p class="top-paragraph">
                     <a href="mailto:info@crusoe.info">info@crusoe.info</a>
                 </p>
                 <p>Tel. 500 503 888</p>
                 </div>            
-                <div class="col-lg-4 col-sm-4">
+                <div class="col-lg-4 col-sm-4 footer-headers">
                 <h2>Address</h2>
                 <p class="top-paragraph">ul. Piłsudskiego 11</p>
                 <p>Kraków, PL 33-323</p>
 
                 </div>            
-                <div class="col-lg-4 col-sm-4">
+                <div class="col-lg-4 col-sm-4 footer-headers">
                 <h2>We accept</h2>
                 <p class="top-paragraph">
                     <ul>
--- index.php
+++ index.php
@@ -43,6 +43,45 @@
 	</section>
 	
 	<!---------icon bar end-------->
+	<!---------about-homepage start-------->
+	<section id="about-homepage">
+	    <div id="about-homepage-content">
+	        <h1>We provide the best first class luxury apartments in Kraków</h1>
+	        <p>Freshly renovated spacious apartment situated in the center of the historic district in Krakow. The Crusoe apartment combines fine modern furnishings and state of the art appliances with the special historical features of original 19th century brick walls and uniquely patterned ceramic tiles floors, which makes staying at Crusoe apartment one of a kind extraordinary experience.
+	        </p>
+	        <p>Crusoe apartment easily accommodates 4 adults and it is located on the second floor of the beautifully renovated historical building on Pilsudski Street within walking distance to the Market Square, Wawel Castle, major restaurants and vibrant nightlife.
+            There is public transportation right in front of the building and there is an elevator in the building. The apartment has 55 square meters and consists of large living room with the 47” TV and sleeping sofa for 2 adults, bedroom with queen size bed, luxurious bathroom with bidet, rain shower and bath essentials and fully equipped kitchen.  
+            We offer complimentary mineral water, tea, coffee, beer, snacks and fresh fruit.  We also welcome our guests with a high quality bottle of wine.  In addition, we provide free delicious breakfast that is served at the charming Café Bon Jour located 2 doors down from the apartment building.
+            </p>
+	    </div>
+	</section>
+	
+	
+	<!---------about-homepage end-------->
+	<section id="pros">
+	    <div class="container-fluid">
+	        <div class="row">
+	            <h2>Why book with us</h2>
+	            <li>
+	                <ul>3 minutes walking distance to Main Square</ul>
+	                <ul>5 minutes walking distance to Wawel</ul>
+	                <ul>public transportation in front of the building</ul>
+	                <ul>historic district </ul>
+	                <ul>breakfast included </ul>
+	                <ul>complimentary drinks and snacks</ul>
+	                <ul>free WiFi + free WiFi To Go</ul>
+	            </li>
+	        </div>
+	        <div class="row">
+	            <div>
+	                <img src="http://localhost/blog_projekt/wp-content/themes/crusoe theme/img/crusoelogo.png" alt="">
+	            </div>
+	        </div>
+	        <div class="row">
+	            
+	        </div>
+	    </div>
+	</section>
  
 <?php if( have_posts() ) : ?>
 	<?php while( have_posts() ) : the_post() ?>

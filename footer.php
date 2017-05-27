1e08159bb31ee690e2694d2a8cdbe590edaad356
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-05-27 15:49:34 +0200

    footer - in progress

--- footer.php
+++ footer.php
@@ -1,15 +1,29 @@
 <section id="footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-4 col-sm-6">
-            
-            </div>            
-            <div class="col-lg-4 col-sm-6">
-            
-            </div>            
-            <div class="col-lg-4 col-sm-6">
-            
+            <div id="footer-bar">
+                <div class="col-lg-4 col-sm-4">
+                <h2>Contact Us</h2>
+                <p class="top-paragraph">
+                    <a href="mailto:info@crusoe.info">info@crusoe.info</a>
+                </p>
+                <p>Tel. 500 503 888</p>
+                </div>            
+                <div class="col-lg-4 col-sm-4">
+                <h2>Address</h2>
+                <p class="top-paragraph">ul. Piłsudskiego 11</p>
+                <p>Kraków, PL 33-323</p>
+
+                </div>            
+                <div class="col-lg-4 col-sm-4">
+                <h2>We accept</h2>
+                <p class="top-paragraph"></p>
+                </div>
             </div>
+        <div id="copyright" class="col-lg-6 col-md-6 col-sm-6">
+            <p>© 2017 by www.crusoe-apartaments-krakow.com</p>
+            <p>Web development Krzysztof Sokalla</p>
+        </div>
         </div>
     </div>
 </section>

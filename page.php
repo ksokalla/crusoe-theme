a86955b8d3df531a6e5d6e0450e7b6472929a593
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-05-28 10:04:09 +0200

    styling pages init

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php get_header() ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<?php the_title(); ?></h1>  
+        <div class="entry">
+            <?php the_content(); ?>
+        </div>
+<?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer() ?>
+	

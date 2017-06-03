61f39ff90d1867daaddc15e5ce039ea7b22aa97d
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-06-03 23:18:42 +0200

    final asdjustments

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<h1><?php the_title(); ?></h1>  
+<h1 class="page-heading"><?php the_title(); ?></h1>  
         <div class="entry">
             <?php the_content(); ?>
         </div>

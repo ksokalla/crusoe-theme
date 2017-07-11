b06fc60ddd645bb2d309a8a8ab7e4f34c6fd116a
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-07-11 07:22:27 +0200

    more fixes

--- page-apartments.php
+++ page-apartments.php
@@ -1,12 +1,12 @@
 <?php
 /*
  * Template Name: Apartments
- * Description: Contact page template
+ * Description: Apartments page template
  */
  get_header() ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <h1 class="page-heading"><?php the_title(); ?></h1>
-        <div class="entry apartments-page-content-container">
+        <div class="entry apartments-page-content-container" id="apartments-background-fix">
             <?php the_content(); ?>
             </div>
             
